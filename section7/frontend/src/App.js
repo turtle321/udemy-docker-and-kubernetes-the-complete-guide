4d5d54914f230c03fe487fcf3dfd3f00f064b799
turtle321 <turtleturtle885@gmail.com>
2019-08-15 19:25:05 +0200

    NEW Change src to test deployment

--- section7/frontend/src/App.js
+++ section7/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world, how are you doing?</p>
+        <p>Hello world, how are you doing section7?</p>
         <a
           className="App-link"
           href="https://reactjs.org"

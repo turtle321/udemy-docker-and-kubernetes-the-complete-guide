1b9f524ee83261005fd5280be115313f2f195602
turtle321 <turtleturtle885@gmail.com>
2019-08-22 17:49:05 +0200

    NEW Edit src to test deployment

--- section8-9-10/complex/client/src/App.js
+++ section8-9-10/complex/client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib calculator</h1>
             <div style={{ color: "white" }}>
               <Link className="naked" to="/">
                 Home

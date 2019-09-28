af3da9d616419fc845f930192280e05eec448ae0
turtle321 <turtleturtle885@gmail.com>
2019-09-28 13:36:29 +0200

    NEW Sections 12 and 13

--- section8-9-10-11/complex/client/src/OtherPage.js
+++ section8-9-10-11/complex/client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default () => {
   return (
     <div>
-      <p>Im some other page</p>
+      <p>Im some other page at v2</p>
       <p>
         <Link className="naked" to="/">
           Go back to home page!

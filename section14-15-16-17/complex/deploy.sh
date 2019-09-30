e58b3ef3aefbb3cabc41e63a875ed43dcc70a704
turtle321 <turtleturtle885@gmail.com>
2019-09-30 18:28:29 +0200

    NEW Add deployment to GCS

--- section14-15-16-17/complex/deploy.sh
+++ section14-15-16-17/complex/deploy.sh
@@ -3,12 +3,12 @@
 echo "DEPLOYING..."
 
 # Apply all kubernetes (declarative) config.
-###kubectl apply -f k8s
+kubectl apply -f k8s
 
 # Imperative command to use the latest image.
 # Note: this is the best way to use an updated Docker Hub image.
 # The alternative would be editing the config files in the `k8s` dir
 # which is indeed not feasible in a automatic deployment.
-###kubectl set image deployments/server-deployment server=nimiq/multi-server:$SHA
-###kubectl set image deployments/client-deployment client=nimiq/multi-client:$SHA
-###kubectl set image deployments/worker-deployment worker=nimiq/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=nimiq/multi-server:$SHA
+kubectl set image deployments/client-deployment client=nimiq/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=nimiq/multi-worker:$SHA
